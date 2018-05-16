779bda32513c3182a65b919bb4e96f87859b5b87
Alex Sell <alextricityy@gmail.com>
2018-05-16 11:55:25 +0100

    20 failures

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
-  belongs_to :Restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: {only_integer: true}
+
+  belongs_to :restaurant
   # resources: Review
   Review :resources
 end
---
+++ db/migrate/20180516103556_change_tables_in_reviews.rb
@@ -0,0 +1,4 @@
+class ChangeTablesInReviews < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
